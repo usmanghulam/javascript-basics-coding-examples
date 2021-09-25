0a7649ba6130215d9690090c6c0fcacbc14b67e0
Usman Ghulam <usmanghulam01@gmail.com>
2021-09-25 17:17:45 +0500

    25 Sep 2021

--- basics.js
+++ basics.js
@@ -172,3 +172,116 @@ function abc(str1="aabc", str2="aaab"){
 
 // 15. Write a JavaScript program to get the difference between a given number and 13,
 //  if the number is greater than 13 return double the absolute difference.
+const sumDouble = n => {
+    const num = 13;
+    if (n < num) {
+        return num - n;
+    } else {
+        return Math.abs((num - n) * 2);
+    }
+}
+
+// 16. Write a JavaScript program to compute the sum of the two given integers. If the two values are same, then returns 
+// triple their sum.  Go to the editor.
+
+const sumTriple = (n1, n2) => {
+    if (n1 === n2) {
+        return (n1 + n2) * 3;
+    }
+    return n1 + n2;
+}
+
+// 17. Write a JavaScript program to compute the absolute difference between a specified number and 19. 
+// Returns triple their absolute difference if the specified number is greater than 19.
+
+const sumTriple2 = n => {
+    if (n > 19) {
+        return (n *3);
+    }
+    return (19 - n);
+}
+
+// 18. Write a JavaScript program to check two given numbers and return true if one of the number is 50 or if their sum is 50.
+
+const SumOREqual50 = (n1, n2) => {
+    if (n1 === 50 || n2 === 50 || (n1 + n2) === 50) {
+        return true
+    }
+    return false;
+}
+
+// 21. Write a JavaScript program to create a new string adding "Py" in front of a given string. If the given 
+// string begins with "Py" then return the original string.
+const createString = str => {
+    if ("Py" === str.substring(0, 2)) {
+        return str;
+    }
+    return `Py${str}`;
+}
+
+// 22. Write a JavaScript program to remove a character at the specified position of a given string and return the new string.  
+const removeCharString = (string="usmanghulam", index=5) => {
+    let newString = string.split("");
+    newString.splice(index, 1);
+    return newString.join().replaceAll(",","");
+}
+
+// 23. Write a JavaScript program to create a new string from a given string changing the position of first and last characters. 
+// The string length must be greater than or equal to 1.
+const changeString = str => {
+    if (str.length > 1) {
+        let charFirst = str.charAt(0);
+        let charLast = str.charAt(str.length - 1);
+        let newString = str.split("");
+        newString.shift();
+        newString.pop();
+        newString.unshift(charLast);
+        newString.push(charFirst);
+        return newString.join().replaceAll(",", "");
+    }
+    return "Invalid String";
+}
+
+// 24. Write a JavaScript program to create a new string from a given string with the first character of the given string added at the front and back.
+const frontBack = str => {
+    let stringSplit = str.split("");
+    let firstChar = str.charAt(0);
+    stringSplit.unshift(firstChar);
+    stringSplit.push(firstChar);
+    return stringSplit.join().replaceAll(",", "");
+}
+
+// 25. Write a JavaScript program to check whether a given positive number is a multiple of 3 or a multiple of 7.
+const multipleOF37 = n => {
+    if (n % 3 === 0 || n % 7 === 0) {
+        return true
+    }
+    return false;
+}
+
+// 28. Write a JavaScript program to check whether two given integer values are in
+//  the range 50..99 (inclusive). Return true if either of them are in the said range.
+
+const rangeNumber = n => {
+    if (n >= 50 && n <= 99) {
+        return true;
+    }
+    return false;
+}
+
+// QUICK ALGO
+function stringFun(str="usmanghulam") {
+    let obj = {};
+    for(let i=0; i<str.length; i++) {
+        if (obj[str.charAt(i)] === 1) {
+           obj[str.charAt(i)] = obj[str.charAt(i)] + 1
+        } else {
+            obj[str.charAt(i)] = 1;
+        }
+    }
+    return obj;
+}
+
+// 30. Write a JavaScript program to check whether a string "Script" presents at 5th (index 4) position in a given string, if "Script" 
+// presents in the string return the string without "Script" otherwise return the original one. 
+
